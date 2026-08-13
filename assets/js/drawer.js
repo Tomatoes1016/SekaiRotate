d99178a20a19f8ef11dafbd52c642e77a4559d0d
Ziqi Chen <ch220920@outlook.com>
2026-08-13 23:32:21 +0800

    使用Material Web Components重构了页面，并对文件结构进行了调整。

---
+++ assets/js/drawer.js
@@ -0,0 +1,56 @@
+const charactList = [
+    { value: 'emu', label: 'emu' },
+    { value: 'tks', label: 'tks' },
+    { value: 'rui', label: 'rui' },
+    { value: 'nene', label: 'nene' }
+];
+
+const characterImg = document.getElementById('character');
+const settingsFab = document.getElementById('settingsFab');
+const drawer = document.getElementById('drawer');
+const drawerOptions = document.getElementById('drawerOptions');
+const drawerBtn = document.getElementById('drawerBtn');
+
+function render() {
+    if (!drawerOptions) return;
+    const selectedValue = 'emu';
+    drawerOptions.innerHTML = charactList
+        .map(item => {
+            const isChecked = item.value === selectedValue ? 'checked' : '';
+
+            return `
+            <label>
+                <md-radio
+                    class="drawer-item"
+                    id="${item.value}"
+                    name="character"
+                    value="${item.value}" 
+                    touch-target="wrapper"
+                    ${isChecked}></md-radio>
+                <span aria-hidden="true">${item.label}</span>
+            </label>
+        `;
+        })
+        .join('');
+}
+
+render();
+
+settingsFab?.addEventListener('click', () => {
+    drawer.open = true;
+});
+
+drawerBtn?.addEventListener('click', () => {
+    const Radios = Array.from(drawerOptions.querySelectorAll('md-radio'));
+    const checkedRadio = Radios.find(radio => radio.checked);
+
+    if (checkedRadio && characterImg) {
+        const selectedCharacter = checkedRadio.id;
+
+        characterImg.src = `./assets/images/${selectedCharacter}.png`;
+        characterImg.alt = selectedCharacter;
+
+        console.log('Character switched to:', selectedCharacter)
+    }
+    drawer.open = false;
+});
---
+++ assets/js/material-color.js
@@ -0,0 +1,26 @@
+import { argbFromHex, themeFromSourceColor, applyTheme, sourceColorFromImage } from "./material-color-utilities.js";
+
+const character = document.getElementById('character');
+
+async function updateTheme() {
+    if (!character) return;
+    try {
+        const sourceColor = await sourceColorFromImage(character);
+        const theme = themeFromSourceColor(sourceColor);
+
+        console.log(JSON.stringify(theme, null, 2));
+
+        const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        applyTheme(theme, { target: document.body, dark: systemDark });
+    } catch (ererror) {
+        console.error('Failed to update theme', error);
+
+    }
+}
+
+if (character) {
+    character.addEventListener('load', updateTheme);
+    if (character.complete) {
+        updateTheme();
+    }
+}
